fe2e7a8e5c60fd0b805aa83a0130086338aaae68
pastelhub1234-pixel <pastelhub1234@gmail.com>
2025-12-27 02:50:52 +0900

    Update TodoList.tsx

--- src/features/home/components/TodoList.tsx
+++ src/features/home/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'; 
 import { CheckSquare, ExternalLink } from 'lucide-react';
 import { useJsonData } from '../../../hooks/useJsonData';
 
